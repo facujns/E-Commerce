344783b1fbfdd73f10e6536c1820e1e8c0d86036
chuky200 <fanuso14@hotmail.com>
2022-11-26 21:47:02 -0300

    Entrega 5
    v5

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,139 @@
+let producto = {};
+let precioUni = 0;
+let cantidad = 0
+let subtotal = 0;
+let costoEnvio = 0;
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          paywayError();
+          event.preventDefault()
+          event.stopPropagation()
+                
+        }
+      
+        else if(form.checkValidity()){
+
+             document.getElementById('successMessage').classList.replace('d-none','d-block');
+   }
+          form.classList.add('was-validated')        
+    
+  }, false)
+
+     })
+})()
+
+fetch(CART_INFO_URL + "25801" + EXT_TYPE)
+.then(resp => resp.json())
+.then(data => {
+producto = data;
+console.log(producto)
+  showArticulo(producto)
+  tipoEnvio();
+  costos();
+
+
+})
+
+function showArticulo(){
+
+  precioUni = producto.articles[0].unitCost;
+  listaDesplegable = `
+  <td><img class="img-thumbnail" src="${producto.articles[0].image}" style="width:100px"</td>
+  <td>${producto.articles[0].name}</td>
+  <td>${producto.articles[0].currency} ${producto.articles[0].unitCost}</td>
+  <td> <input type="number" id="articleCount" value=1 onchange="subTotal(),precioFinal()" min="1" style="width:50px;"></td>
+  <td id="subtotalCost"><strong>${producto.articles[0].currency} ${precioUni}</strong></td>
+  `
+
+  document.getElementById('tableInfo').innerHTML = listaDesplegable;
+}
+
+
+function subTotal(){
+  precioUni = producto.articles[0].unitCost;
+  cantidad = document.getElementById('articleCount').value;
+  subtotal = precioUni * cantidad ;
+  subtotalFinal = producto.articles[0].currency + " " + subtotal;
+  document.getElementById('subtotalCost').innerHTML =    `<strong>${producto.articles[0].currency} ${subtotal}</strong>  ` ;
+  document.getElementById('subtotalFinal').innerHTML =  subtotalFinal;
+
+}
+
+function tipoEnvio(){
+   cantidad = document.getElementById('articleCount').value;
+  
+  subtotal = precioUni * cantidad ;
+  premium = subtotal * 0.15;
+  express = subtotal * 0.07;
+  standard = subtotal * 0.05;
+  costoEnvio = premium;
+  document.getElementById('costoEnvio').innerHTML = producto.articles[0].currency + " " + costoEnvio;
+  document.getElementById('precioTotal').innerHTML = producto.articles[0].currency +" " + (subtotal + costoEnvio) ;
+
+  document.getElementById('premium').addEventListener('click', function(){
+  costoEnvio = premium;
+  document.getElementById('costoEnvio').innerHTML = producto.articles[0].currency + " " + costoEnvio;
+  document.getElementById('precioTotal').innerHTML = subtotal + costoEnvio;
+  })
+  document.getElementById('express').addEventListener('click', function(){
+  costoEnvio = express;
+  document.getElementById('costoEnvio').innerHTML =  producto.articles[0].currency + " " +  costoEnvio;
+  document.getElementById('precioTotal').innerHTML = producto.articles[0].currency +" " + (subtotal + costoEnvio) ;
+  })
+  document.getElementById('standard').addEventListener('click', function(){
+  costoEnvio = standard;
+  document.getElementById('costoEnvio').innerHTML = producto.articles[0].currency + " " +  costoEnvio;
+  document.getElementById('precioTotal').innerHTML = producto.articles[0].currency +" " + (subtotal + costoEnvio) ;
+})
+}
+
+function costos(){
+  precioUni = producto.articles[0].unitCost;
+  cantidad = document.getElementById('articleCount').value;
+  subtotal = precioUni * cantidad ;
+  costoEnvio = premium;
+  subtotalFinal = producto.articles[0].currency + " " + subtotal;
+  document.getElementById('subtotalFinal').innerHTML = subtotalFinal;
+  
+   
+}
+
+
+let creditCard = document.getElementById('creditCard');
+creditCard.addEventListener('click',function(){
+
+  document.getElementById('creditField').removeAttribute('disabled',"")
+  document.getElementById('bankField').setAttribute('disabled',"")
+  document.getElementById('paywaymethod').innerHTML = "Tarjeta de Crédito";
+})
+let bankTransfer = document.getElementById('bankTransfer')
+bankTransfer.addEventListener('click',function(){
+  document.getElementById('bankField').removeAttribute('disabled',"")
+  document.getElementById('creditField').setAttribute('disabled',"")
+  document.getElementById('paywaymethod').innerHTML = "Transferencia bancaria";
+})
+  
+function paywayError(){
+  
+  if(!creditCard.checked && !bankTransfer.checked){
+    document.getElementById('paywayError').innerHTML = "Debes seleccionar un método de pago";
+  }
+else if(creditCard.checked || bankTransfer.checked){
+    document.getElementById('paywayError').innerHTML = "";
+  }
+}
+  
+  
   // MOSTRAR PERFIL // BOTON INICIO DE SESION
   sesionOn();
   function showUser(){
--- js/product-info.js
+++ js/product-info.js
@@ -1,34 +1,18 @@
-const prodID = localStorage.getItem('prodID');
+const prodID = localStorage.getItem("prodID");
 
-
-                                //  INFORMACION DEL PRODUCTO
+//  INFORMACION DEL PRODUCTO
 fetch(PRODUCT_INFO_URL + prodID + EXT_TYPE)
-.then(resp => resp.json())
-.then(data => {
+  .then((resp) => resp.json())
+  .then((data) => {
+    var añadirAlCarrito = document.getElementById('añadirCarrito')
+     añadirAlCarrito.onclick = function(){
+      console.log('click');
+     }
+    document.getElementById("container").innerHTML += showCarrousel();
 
-document.getElementById('container').innerHTML += `<div class="list-group-item infoYcomm">
-<div class="row">
-<div class="col-12"><h3>${data.name}</h3>
-<hr>
-<strong>Precio</strong>
-<p>${data.currency} ${data.cost}</p>
-<strong>Descripción</strong>
-<p>${data.description}</p>
-<strong>Categoria</strong>
-<p>${data.category}</p>
-<strong>Cantidad de vendidos</strong>
-<p>${data.soldCount}</p>
-<strong>Imagenes Ilustrativas</strong>
-</div>    
-</div>
-</div>
-`
-document.getElementById('container').innerHTML += showCarrousel();
-
-
-//  CARROUSEL DE IMAGENES
-function showCarrousel(){
-    let carrousel = ` <div id="carouselExampleControls" class="carousel slide w-75 container" data-bs-ride="carousel" style="max-height:400px;">
+    //  CARROUSEL DE IMAGENES
+    function showCarrousel() {
+      let carrousel = ` <div id="carouselExampleControls" class="carousel slide " data-bs-ride="carousel">
 <button class="carousel-control-prev bg-secondary" type="button"style="width:8%;" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
 <span class="carousel-control-prev-icon  aria-hidden="true"></span>
 <span class="visually-hidden">Previous</span>
@@ -37,162 +21,196 @@ function showCarrousel(){
 <span class="carousel-control-next-icon" aria-hidden="true"></span>
 <span class="visually-hidden">Next</span>
 </button>
-<div class="carousel-inner" >
-    <div class="carousel-item active" style="height:400px;">
-      <img src="${data.images[0]}" class="d-block m-auto" style="height:100%;width:100%;"  alt="...">
-    </div>`
-    for(i=1;i< data.images.length;i++){
-    carrousel += 
-    `
+
+<div class="carousel-inner ">
+    <div class="carousel-item active  ">
+      <img src="${data.images[0]}" class="d-block w-50 m-auto rounded-circle" alt="...">
+    </div>`;
+      for (i = 1; i < data.images.length; i++) {
+        carrousel += `
     
-     <div class="carousel-item " style="height:400px;">    
-      <img src="${data.images[i]}" class="d-block m-auto" style="height:100%;width:100%;"alt="...">
-    </div>`}
-    carrousel += `
+     <div class="carousel-item">
+      <img src="${data.images[i]}" class="d-block w-50 m-auto rounded-circle" alt="...">
+    </div>`;
+      }
+      carrousel += `
   </div>
-</div>`
+</div>`;
+
+      return carrousel;
+    }
+
+    console.log(data.images.length);
+
+    // PRODUCTOS RELACIONADOS
+    document.getElementById("relatedProds").innerHTML += `<hr>
+<h4>Productos relacionados</h4>`;
+
+    for (relatedProds of data.relatedProducts) {
+      document.getElementById("relatedProds").innerHTML += `
+
+
+<div class="col-4">
+
+<img onclick="setProdID(${relatedProds.id})"class="card-img-top col-4" style="width: 19rem;" src="${relatedProds.image}" alt="Card image cap">
+<h5 class="ms-1">${relatedProds.name}</h5>  
+
+
+</div> `;
+    }
     
-return carrousel
 
-}
-
-
-   
-
-// PRODUCTOS RELACIONADOS
-document.getElementById('relatedProds').innerHTML += `<hr>
-<h4>Productos relacionados</h4>`
-
-for(relatedProds of data.relatedProducts){
-    document.getElementById('relatedProds').innerHTML += `
-
-
-<div class="col">
-
-<img onclick="setProdID(${relatedProds.id})"class="card-img-top" style="width: 19rem; cursor:pointer;" src="${relatedProds.image}" alt="Card image cap" onclick="setProdID()">
-<h5 class="ms-1">${relatedProds.name}</h5>
-
-
-</div> ` 
-}
-
-})
+  });
 
 // FETCH DE LOS COMENTARIOS
-const commentBtn = document.getElementById('comment-btn');
-const textArea = document.getElementById('product-info-textarea');
-const prodScore = document.getElementById('comment-select');
+const commentBtn = document.getElementById("comment-btn");
+const textArea = document.getElementById("product-info-textarea");
+const prodScore = document.getElementById("comment-select");
 
 fetch(PRODUCT_INFO_COMMENTS_URL + prodID + EXT_TYPE)
-.then(resp => resp.json())
-.then(data => {  
+  .then((resp) => resp.json())
+  .then((data) => {
+    document.getElementById("container2").innefurHTML += `
+<h4>Comentarios</h4>`;
 
-document.getElementById('container2').innefurHTML +=`
-<h4>Comentarios</h4>`
-
-for(let comment of data){ 
-    document.getElementById('container2').innerHTML +=`
+    for (let comment of data) {
+      document.getElementById("container2").innerHTML += `
         <div class="list-group-item infoYcomm">
         <div class="row">
         <div class="col-12">
-        <strong>${comment.user}</strong> - ${comment.dateTime} - ${showStars(comment.score)}
+        <strong>${comment.user}</strong> - ${comment.dateTime} - ${showStars(
+        comment.score
+      )}
         <p>${comment.description}</p>
         </div>
         </div>
-        </div>`
-    
+        </div>`;
     }
 
-// AGREGAR COMENTARIOS
-    commentBtn.addEventListener('click', function crearComentario(){
-            console.log(textArea.value);
-            console.log(prodScore.value);
-            console.log(localStorage.getItem('user'));
-            
-            let today = new Date();
-            const fecha = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
-            const hora =  `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
-            if((textArea.value != "") && (localStorage.getItem('user') != null || sessionStorage.getItem('user') != null)){ 
-            document.getElementById('container2').innerHTML +=`
+    // AGREGAR COMENTARIOS
+    commentBtn.addEventListener("click", function crearComentario() {
+      console.log(textArea.value);
+      console.log(prodScore.value);
+      console.log(localStorage.getItem("user"));
+
+      let today = new Date();
+      const fecha = `${today.getFullYear()}-${
+        today.getMonth() + 1
+      }-${today.getDate()}`;
+      const hora = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+      if (
+        textArea.value != "" &&
+        (localStorage.getItem("user") != null ||
+          sessionStorage.getItem("user") != null)
+      ) {
+        document.getElementById("container2").innerHTML += `
             <div class="list-group-item" style="background-color:#EAF5F0; border: 1px solid black;">
             <div class="row" style="background-color:#EAF5F0">
             <div class="col-12">
-            <strong>${localStorage.getItem('user') || sessionStorage.getItem('user')}</strong> - ${fecha} ${hora} - ${showStars(prodScore.value)}
+            <strong>${
+              localStorage.getItem("user") || sessionStorage.getItem("user")
+            }</strong> - ${fecha} ${hora} - ${showStars(prodScore.value)}
             <p>${textArea.value}</p>
             </div>
             </div>
-            </div>`
-        }
-        showAlerts(); 
-    })  
-})
-function showAlerts(){
-    
-    if(localStorage.getItem('user') == null && sessionStorage.getItem('user') == null ){
-       
-         alertify.alert('<i class="fa fa-close" style="font-size:30px;color:red;"></i><strong style="font-size:23px;"> Error al comentar</strong>', '<p style="font-size: 18px;">Debes iniciar sesión para poder comentar</p>', 'Iniciar Sesion');
-
-     }
-    else if((localStorage.getItem('user') != null || sessionStorage.getItem('user') != null) && (textArea.value == '')){
-        document.getElementById('product-info-textarea').style.border = "solid 2px red"
-        document.getElementById('alertError').style.display = "block"
-        document.getElementById('alertError').style.color = "red"
-        }
-        
+            </div>`;
+      }
+      showAlerts();
+    });
+  });
+function showAlerts() {
+  if (
+    localStorage.getItem("user") == null &&
+    sessionStorage.getItem("user") == null
+  ) {
+    alertify.alert(
+      '<i class="fa fa-close" style="font-size:30px;color:red;"></i><strong style="font-size:23px;"> Error al comentar</strong>',
+      '<p style="font-size: 18px;">Debes iniciar sesión para poder comentar</p>',
+      "Iniciar Sesion"
+    );
+  } else if (
+    (localStorage.getItem("user") != null ||
+      sessionStorage.getItem("user") != null) &&
+    textArea.value == ""
+  ) {
+    document.getElementById("product-info-textarea").style.border =
+      "solid 2px red";
+    document.getElementById("alertError").style.display = "block";
+    document.getElementById("alertError").style.color = "red";
+  }
 }
-    document.getElementById('product-info-textarea').addEventListener('click', function(){
-        document.getElementById('alertError').style.display = "none"
-        document.getElementById('product-info-textarea').style.border = "solid 1px black"
-
-    })
+document
+  .getElementById("product-info-textarea")
+  .addEventListener("click", function () {
+    document.getElementById("alertError").style.display = "none";
+    document.getElementById("product-info-textarea").style.border =
+      "solid 1px black";
+  });
 
 // FUNCION PARA MOSTRAR LAS ESTRELLAS
-function showStars(cantidad){
-    let starsHTML = "";
-    for(i=0;i<cantidad;i++){
-        starsHTML += `<span class="fa fa-star checked"></span>`;
-    }
-    for(i=cantidad; i < 5;i++){
-        starsHTML += `</span> <span class="fa fa-star"></span>`;
-    }
-    return starsHTML
+function showStars(cantidad) {
+  let starsHTML = "";
+  for (i = 0; i < cantidad; i++) {
+    starsHTML += `<span class="fa fa-star checked"></span>`;
+  }
+  for (i = cantidad; i < 5; i++) {
+    starsHTML += `</span> <span class="fa fa-star"></span>`;
+  }
+  return starsHTML;
 }
-function setProdID(id){
-    localStorage.setItem('prodID', id );
-    window.location = "product-info.html"
-  }                       
-
-
-
-  // MOSTRAR PERFIL // BOTON INICIO DE SESIONJ
-sesionOn();
-function showUser(){
-    document.getElementById('showUser').innerHTML = 
+function setProdID(id) {
+  localStorage.setItem("prodID", id);
+  window.location = "product-info.html";
+}
+function addCartProduct(data) {
+  
+  console.log(data)
+  var productos = JSON.parse(localStorage.getItem("cartProduct"));
+  if (productos === undefined){
+    localStorage.setItem('cartProduct', JSON.stringify([data]))
+  }
+  else{
+    productos.push(data)
+    localStorage.setItem('cartProduct', JSON.stringify(data));
     
-    ` <a class="nav-link dropdown-toggle text-dark fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    ${localStorage.getItem('user') || sessionStorage.getItem('user')}
+  }
+  
+  
+  
+  window.location = "cart.html";
+}
+
+
+
+
+// MOSTRAR PERFIL // BOTON INICIO DE SESIONJ
+sesionOn();
+function showUser() {
+  document.getElementById(
+    "showUser"
+  ).innerHTML = ` <a class="nav-link dropdown-toggle text-dark fw-bold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    ${localStorage.getItem("user") || sessionStorage.getItem("user")}
     </a>
     <ul class="dropdown-menu bg-light">
     <li><a class="dropdown-item text-dark" href="./cart.html">Carrito</a></li>
     <li><a class="dropdown-item text-dark" href="./my-profile.html">Mi perfil</a></li>
     <li><hr class="dropdown-divider"></li>
     <li><a class="dropdown-item text-dark" href="#" onclick="cerrarSesion()">Cerrar Sesión</a></li>
-    </ul>`
-    
-    }
-function sesionOn(){
-if(localStorage.getItem('user') || sessionStorage.getItem('user'))
-showUser();
-else{
-document.getElementById('showUser').innerHTML = 
-`<button type="button" class="button-15" id="indexBtn" role="button">Iniciar Sesion</button>`
-document.querySelector('#indexBtn').addEventListener('click', function(){
-window.location.href = './login.html';
-})
-}}
-function cerrarSesion(){
-localStorage.removeItem('user') , sessionStorage.removeItem('user');
-location.reload();
-};     
-
-
+    </ul>`;
+}
+function sesionOn() {
+  if (localStorage.getItem("user") || sessionStorage.getItem("user"))
+    showUser();
+  else {
+    document.getElementById(
+      "showUser"
+    ).innerHTML = `<button type="button" class="button-15" id="indexBtn" role="button">Iniciar Sesion</button>`;
+    document.querySelector("#indexBtn").addEventListener("click", function () {
+      window.location.href = "./login.html";
+    });
+  }
+}
+function cerrarSesion() {
+  localStorage.removeItem("user"), sessionStorage.removeItem("user");
+  location.reload();
+}
